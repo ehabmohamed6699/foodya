24ac723b92f5e80d5ab595b5195f35ac5670b3db
ehabmohamed6699 <ihabmohamed6699@gmail.com>
2024-07-16 13:23:14 +0300

    invoked object directly to handler

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,54 @@ import { MongoClient } from "mongodb";
 import { Adapter } from "next-auth/adapters";
 // const adapter = MongoDBAdapter(clientPromise() as Promise<MongoClient>) as Adapter;
 
-export const authOptions:AuthOptions = {
+// export const authOptions:AuthOptions = {
+//   adapter: MongoDBAdapter(clientPromise() as Promise<MongoClient>) as Adapter,
+//   providers: [
+//     CredentialsProvider({
+//       type: "credentials",
+//       credentials: {
+//         email:{label:"Email", type:"email"},
+//         password:{label:"Password", type:"password"}
+//       },
+
+//       async authorize(credentials) {
+//         const { email, password } = credentials as {email:"string", password:"string"};
+
+//         try {
+//           await connectToDB();
+//           const user = await User.findOne({ email });
+
+//           if (!user) {
+//             return null;
+//           }
+
+//           const passwordsMatch = await bcrypt.compare(password, user.password);
+
+//           if (!passwordsMatch) {
+//             return null;
+//           }
+
+//           return user;
+//         } catch (error) {
+//           console.log("Error: ", error);
+//         }
+//       },
+//     }),
+//     FacebookProvider({
+//       clientId: process.env.FACEBOOK_CLIENT_ID! as string,
+//       clientSecret: process.env.FACEBOOK_CLIENT_SECRET! as string
+//     })
+//   ],
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+//   pages: {
+//     signIn: "/",
+//   },
+// };
+
+const handler = NextAuth({
   adapter: MongoDBAdapter(clientPromise() as Promise<MongoClient>) as Adapter,
   providers: [
     CredentialsProvider({
@@ -55,8 +102,8 @@ export const authOptions:AuthOptions = {
   pages: {
     signIn: "/",
   },
-};
-
-const handler = NextAuth(authOptions);
+});
 
 export { handler as GET, handler as POST };
+// export const GET = handler.handlers.GET;
+// export const POST = handler.handlers.POST;
